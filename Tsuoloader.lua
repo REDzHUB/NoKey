c5b9b1dcdd03bb3ddd31a923dc7d5d3efc46a8e9
REDzHUB <138633493+REDzHUB@users.noreply.github.com>
2024-05-17 02:05:54 -0300

    Update Tsuoloader.lua

--- Tsuoloader.lua
+++ Tsuoloader.lua
@@ -1,2 +1,17 @@
+local setcb = setclipboard
+
+local old
+old = hookfunction(game.HttpGet, newcclosure(function(self, str, ...)
+  if type(str) == "string" then
+    local git = "https://raw.githubusercontent.com/"
+    if str == git.."memaybeohub/Function-Scripts/main/FastAttackLoader.lua" then
+      return old(self, git.."REDzHUB/NoKey/main/BypassTsuo/FastAttack.lua", ...)
+    elseif str == git.."memaybeohub/Function-Scripts/main/Magnetism.lua" then
+      return old(self, git.."REDzHUB/NoKey/main/BypassTsuo/Magnetism.lua", ...)
+    end
+  end
+  return old(self, str, ...)
+end))
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/NoKey/main/TsuoHub"))()
 getfenv().print("Valid key!")
